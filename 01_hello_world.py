a174978e265dc4f4f7c0d0dbeb31e83d00f21d90
ERICK ADIKAH <104826804+Erickadikah@users.noreply.github.com>
2022-08-15 15:52:27 +0300

    the basics of python

--- 01_hello_world.py
+++ 01_hello_world.py
@@ -1,1 +1,3 @@
-print("Hello world")
+name = "erick adikah"
+age = 23
+print("%s is your age %d" % (name, age))
---
+++ comparison & logcal op.py
@@ -0,0 +1,9 @@
+x = 10
+if x >= 10:
+    print("x is positive")
+    if(x % 2) == 0:
+        print("x is even")
+    else:
+        print("x is odd")
+else:
+    print("x is negative")
