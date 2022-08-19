1645a27349189738b2fc239991926d03597be366
ERICK ADIKAH <104826804+Erickadikah@users.noreply.github.com>
2022-08-19 19:54:10 +0300

    __init__ self functions

---
+++ cars.py
@@ -0,0 +1,19 @@
+class Car:
+    def __init__(self, speed, color):
+        self.__speed = speed
+        self.__color = color
+
+    def set_color(self, value):
+        self.__color = value
+
+        def get_color(self):
+            return self.__color
+
+
+ford = Car(200, 'red')
+honda = Car(250, 'blue')
+audi = Car(300, 'black')
+
+for.set_speed(300)
+print(ford.get_speed())
+print(ford.get_color())
