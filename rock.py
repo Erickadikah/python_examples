cd034e94103eb3b16dddeba29becd2c11579da29
ERICK ADIKAH <104826804+Erickadikah@users.noreply.github.com>
2022-08-20 12:50:22 +0300

    code change

--- rock.py
+++ rock.py
@@ -1,10 +1,17 @@
+import random
+
+
 def get_choices():
-    player_choice = input("Enter a choice (rock, paper, scissors: ")
-    computer_choice = "paper"
-    choices = {"player": player_choice, "computer": computer_choice}
-    return choices
+    player_choice = input("Enter a choice (rock, paper, scissors:")
 
-    choices = get_choices()
-    print(choices)
 
-    dict = {"name": "erick", "color": choices}
+option = ["rock", "paper", "scissors"]
+computer_choice = random.choice(options)
+choices = {"player": player_choice, "computer": computer_choice}
+return choices
+choices = get_choices()
+print(choices)
+
+
+food = ["pizza", "carrot", "eggs"]
+dinner = random.choice(food)
