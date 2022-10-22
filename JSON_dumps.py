651c960f8665e3dd77c61b86004177b2aedd2230
ERICK ADIKAH <104826804+Erickadikah@users.noreply.github.com>
2022-10-22 21:06:56 +0300

    my study

--- JSON_dumps.py
+++ JSON_dumps.py
@@ -2,12 +2,16 @@ import json
 #creating a dictionary
 Dictionary ={1:'Welcome', 2: 'to', 3:'my', 4:'world', 5:'of geek'}
 
+
+
 #our dictionary contains a tuple
 #as key, so it si autoatically
 #skipped if we have not set
 #skipkeys = True the the code
 #throws error
-json_string = json.dumps(Dictionary)
+
+
+json_string = json.dumps(Dictionary,skipkeys = True)
 print('Equivalent json string of input dictionary:',
       json_string)
 print("  ")
